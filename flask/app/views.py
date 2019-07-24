feb34269ce223ea84f44523134e11925799c8ca6
ik4ru52k <ikarus2k@gmx.net>
2019-07-24 19:40:28 +0200

    implemented mounted volumes + sqlalchemy

--- flask/app/views.py
+++ flask/app/views.py
@@ -5,10 +5,10 @@ from app.models import Users
 
 @app.route("/")
 def index():
-    return "***Hello from Flask <3"
+    return "***Hello from Flask hello world <3"
     
 
-@app.route("/users/<username>", methods=["GET", "POST"])
+@app.route("/post/users/<username>", methods=["GET", "POST"])
 def add_user(username):
     password = "test"
     add_user = Users(username, password)
@@ -16,3 +16,15 @@ def add_user(username):
     db.session.commit()
 
     return username
+
+@app.route("/get/users/<username>", methods=["GET", "POST"])
+def get_user(username):
+    get_user=Users.query.filter_by(username=username).first()
+
+    if get_user is None:
+        return f"{username} ####wurde N I C H T gefunden!"
+
+    else:
+        return f"{get_user.username} ####wurde gefunden"
+
+    
